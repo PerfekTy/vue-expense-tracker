a2547cac44eb124cf7816001d52571642f30b8ee
Sebastian <sebastianfelenczak@gmail.com>
2024-03-08 17:19:27 +0100

    working add expenses and income form

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@ import TransitionList from "./components/TrasactionList.vue";
 import AddTrasaction from "./components/AddTransaction.vue";
 
 import { ref, computed } from "vue";
+import { useToast } from "vue-toastification";
 
+const toast = useToast();
 const transactions = ref([
   { id: 1, text: "Flower", amount: -19.99 },
   { id: 2, text: "Salary", amount: 299.97 },
@@ -15,7 +17,9 @@ const transactions = ref([
 ]);
 
 const total = computed(() =>
-  transactions.value.reduce((acc, transaction) => acc + transaction.amount, 0)
+  transactions.value
+    .reduce((acc, transaction) => acc + transaction.amount, 0)
+    .toFixed(2)
 );
 
 const income = computed(() =>
@@ -31,14 +35,28 @@ const expenses = computed(() =>
     .reduce((acc, transaction) => acc + transaction.amount, 0)
     .toFixed(2)
 );
+
+const handleTransactionSubmitted = (transactionData) => {
+  const { text, amount } = transactionData;
+  transactions.value.push({
+    id: generateUniqueId(),
+    text,
+    amount,
+  });
+  toast.success("Toast added successfully.");
+};
+
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1_000_000);
+};
 </script>
 
 <template>
   <HeaderC />
   <div class="container">
-    <Balance :total="total" />
-    <IncomeExpenses :income="income" :expenses="expenses" />
+    <Balance :total="+total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
     <TransitionList :transactions="transactions" />
-    <AddTrasaction />
+    <AddTrasaction @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -20,3 +20,29 @@
     <button class="btn">Add transaction</button>
   </form>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { useToast } from "vue-toastification";
+
+const text = ref("");
+const amount = ref("");
+const toast = useToast();
+const emit = defineEmits(["transactionSubmitted"]);
+
+const onSubmit = () => {
+  if (!text.value || !amount.value) {
+    return toast.error("Both fields must be filled.");
+  }
+
+  const transactionData = {
+    text: text.value,
+    amount: parseFloat(amount.value),
+  };
+
+  emit("transactionSubmitted", transactionData);
+
+  text.value = "";
+  amount.value = "";
+};
+</script>
